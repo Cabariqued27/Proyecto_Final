fb3c6c8b4ee27888b0d81481c21da15a1c774376
David Cabarique <felipe27th@gmail.com>
2023-09-14 22:36:08 -0500

    db development 3.0

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -1,45 +1,35 @@
+import 'package:flutter/material.dart';
 
-import 'package:sirdad/models/event.dart';
-import 'package:sirdad/models/family.dart';
-import 'package:sirdad/models/member.dart';
-import 'package:sirdad/services/database_helper.dart'; // Importa tu clase DataBaseHelper
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+void main() {
+  runApp(MyApp());
+}
 
-void main() async {
-  final dbHelper = DataBaseHelper();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Aplicación Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-  // Crear una familia
-  final newFamily = Family(
-    name: 'Familia Ejemplo',
-    address: '123 Calle Principal',
-  );
-
-  // Insertar la familia en la base de datos
-  final familyId = await dbHelper.insertFamily(newFamily);
-  print('Familia insertada con ID: $familyId');
-
-  // Crear un evento
-  final newEvent = Event(
-    name: 'Evento de Prueba',
-    description: 'Descripción del evento de prueba',
-    date: DateTime.now(),
-  );
-
-  // Insertar el evento en la base de datos
-  final eventId = await dbHelper.insertEvent(newEvent);
-  print('Evento insertado con ID: $eventId');
-
-  // Crear un miembro
-  final family = Family(id: familyId, name: '', address: ''); // Crear una instancia de Family con el ID
-  final newMember = Member(
-    name: 'Miembro de Prueba',
-    age: 30,
-    relationship: 'Hijo',
-    family: family, // Pasar la instancia de Family creada
-  );
-
-  // Insertar el miembro en la base de datos
-  final memberId = await dbHelper.insertMember(newMember);
-  print('Miembro insertado con ID: $memberId');
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: Text(
+          '¡Hola, mundo!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
 }
--- sirdad/lib/services/database_helper.dart
+++ sirdad/lib/services/database_helper.dart
@@ -1,60 +1,29 @@
-import 'package:sirdad/models/event.dart';
+import 'package:sirdad/services/tables.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+class DatabaseS {
+  String name = 'Sirdad';
+  int version = 1;
 
-
-class SirdadDatabase {
-  static final SirdadDatabase instance = SirdadDatabase._init();
-
-  static Database? _database;
-
-  SirdadDatabase._init();
-  final String tableEvent = 'event';
-
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-
-    _database = await _initDB('sirdad.db');
-    return _database!;
+  Future<Database> open()async{
+    String path = join(await getDatabasesPath(),name);
+    return openDatabase(path, 
+    version: version,
+    onConfigure: onConfigure,
+    onCreate: onCreate
+    
+    );
   }
 
-  Future<Database> _initDB(String filePath) async{
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-    return await openDatabase (path, version: 1, onCreate: _onCreateDB);
+  onConfigure(Database DatabaseS) async{
+    await DatabaseS.execute('PRAGMA foreing_keys = ON');
   }
 
-  Future  _onCreateDB(Database db, int version) async {
-    db.execute('''
-          CREATE TABLE family (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            address TEXT
-          )
-        ''');db.execute('''
-          CREATE TABLE $tableEvent (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            description TEXT,
-            date TEXT
-          )
-        ''');db.execute('''
-          CREATE TABLE member (
-            id INTEGER PRIMARY KEY,
-            name TEXT,
-            age INTEGER,
-            relationship TEXT,
-            familyId INTEGER,
-            FOREIGN KEY (familyId) REFERENCES family(id)
-          )
-        ''');
+  onCreate(Database DatabaseS, int version) async {
+    for(var scrip in tables){
+      await DatabaseS.execute(scrip);
+    }
   }
-
-  Future <void> insert(Event event) async{
-    final db = await instance.database;
-    await db.insert(tableEvent,event.toMap());
-  }
-
-  
-        
 }
+
+
