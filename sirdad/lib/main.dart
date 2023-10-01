238d16e269deb61b89493ef1cd0c2721ca62e656
David Cabarique <felipe27th@gmail.com>
2023-10-01 17:32:50 -0500

    First Steps with front end 1.0

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -8,8 +8,6 @@ import 'firebase_options.dart';
 
 
 void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
