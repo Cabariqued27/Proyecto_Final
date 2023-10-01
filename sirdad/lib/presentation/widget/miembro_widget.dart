4de39d1dc832a972789d8c0ed0e0c66f52cfc20b
Hermmann Blanco Serrano <32681730+Hermmann@users.noreply.github.com>
2023-10-01 17:08:39 -0500

    cambio recomendado

--- sirdad/lib/presentation/widget/miembro_widget.dart
+++ sirdad/lib/presentation/widget/miembro_widget.dart
@@ -1,12 +1,8 @@
-// import '/flutter_flow/flutter_flow_checkbox_group.dart';
-// import '/flutter_flow/flutter_flow_theme.dart';
-// import '/flutter_flow/flutter_flow_util.dart';
-// import '/flutter_flow/flutter_flow_widgets.dart';
-// import '/flutter_flow/form_field_controller.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sirdad/presentation/models/miembro_model.dart';
-import 'package:sirdad/presentation/models/member_object.dart';
+import 'package:sirdad/presentation/getters/miembro_model.dart';
+
 
 MiembroModel miembroModel = MiembroModel();
 
