cfd42a5a97cea04c9262e3835def78adc27f53b4
warwolf6432 <jackhowardortega@gmail.com>
2023-10-01 15:14:49 -0500

    db firebase

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -8,6 +8,8 @@ import 'firebase_options.dart';
 
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
   options: DefaultFirebaseOptions.currentPlatform,
 );
