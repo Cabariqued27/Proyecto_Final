537f76faba080c8233c069fc1e8ae29323a41dab
David Cabarique <felipe27th@gmail.com>
2023-10-07 14:26:56 -0500

    firebase

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sirdad/widget/event_widget.dart';
@@ -6,7 +7,9 @@ import 'package:sirdad/widget/miembro_widget.dart';
 
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MainApp());
 }
 
