1d33c9bc77c40897c4998ad69982c2e1bd49fe4c
David Cabarique <felipe27th@gmail.com>
2023-10-10 14:27:24 -0500

    firebase 1.7

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -5,10 +5,10 @@ import 'package:sirdad/widget/event_widget.dart';
 import 'package:sirdad/widget/miembro_widget.dart';
 
 
-void  main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 class MainApp extends StatelessWidget {
   
