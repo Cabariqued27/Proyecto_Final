f8278b62ac5581297b6a2d0619e26872d610652f
Hermmann Blanco Serrano <32681730+Hermmann@users.noreply.github.com>
2023-10-23 11:22:22 -0500

    route familia a miembros

--- sirdad/lib/main.dart
+++ sirdad/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:sirdad/getters/miembro_model.dart';
+import 'package:sirdad/provider/members_provider.dart';
 import 'package:sirdad/widget/family_widget.dart';
 import 'package:sirdad/widget/format_widget.dart';
 import 'package:sirdad/widget/miembro_widget.dart';
@@ -19,13 +20,15 @@ class MainApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (_) => miembroModel),
         ChangeNotifierProvider(create: (_) => familyModel),
+        ChangeNotifierProvider(create: (_) => Members_Provider())
+        // ChangeNotifierProvider(create: (_) => )
 
         //Colocar aquí todos los ChangeNotifierProvider de los modelos que se van a usar
       ],
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.white),
-          home: const FamilyWidget()
+          home: const FormatWidget()
           // ChangeNotifierProvider(
           //   create: (context) => familyModel,
           //   child: const FormatWidget(),
