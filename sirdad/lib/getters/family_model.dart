3157861c666847b24de4f585788e32e13d9a3f2e
Hermmann Blanco Serrano <32681730+Hermmann@users.noreply.github.com>
2023-11-09 16:20:12 -0500

    Update family_model.dart

--- sirdad/lib/getters/family_model.dart
+++ sirdad/lib/getters/family_model.dart
@@ -1,26 +1,46 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
+import 'package:sirdad/models/family.dart';
+import 'package:sirdad/widget/family_widget.dart';
 
-class FamilyModel extends ChangeNotifier {
-  final unfocues = FocusNode();
-  //El Barrio
-  TextEditingController? textController1;
-  String? Function(BuildContext, String?)? textController1Validator;
+class FamilyData extends ChangeNotifier {
+  List<Family> _familys = [];
+  List<Family> get familys => _familys;
 
-  //Dirección
-  TextEditingController? textController2;
-  String? Function(BuildContext, String?)? textController2Validator;
+  Future<void> addFamily(Family family) async {
+    _familys.add(family);
+    await family.save();
+    notifyListeners();
+  }
 
-  //Celular
-  TextEditingController? textController3;
-  String? Function(BuildContext, String?)?
-      textController3Validator; //revisar si corresponde
+  Future<void> getfamilysfb() async {
+    final ref = FirebaseDatabase.instance.ref().child('familys');
+    final snapshot = await ref.get();
 
-  //Fecha
-  TextEditingController? textController4;
-  String? Function(BuildContext, String)? textController4Validator;
+    if (snapshot.exists) {
+      final Map<dynamic, dynamic> data =
+          snapshot.value as Map<dynamic, dynamic>;
 
-  //Jefe del Hogar
-  TextEditingController? textController5;
-  String? Function(BuildContext, String)? textController5Validator;
+      data.forEach((key, value) {
+        String barrio = value['barrio'];
+        String address = value['address'];
+        int phone = value['phone'];
+        String date = value['date'];
+        String jefe = value['jefe'];
+        String eventId = value['eventId'];
+
+        Family newFamily = Family(
+            barrio: barrio,
+            address: address,
+            phone: phone,
+            date: date,
+            jefe: jefe,
+            eventId: eventId);
+        addFamily(newFamily);
+      });
+    } else {
+      print('No data available.');
+    }
+  }
 }
