a1c3d8d5c5e5f944f70f48b208ebd34bf7fd8857
Cabariqued27 <felipe27th@gmail.com>
2023-12-14 15:35:11 -0500

    Update 1.0.2

--- sirdad/lib/widget/event_widget.dart
+++ sirdad/lib/widget/event_widget.dart
@@ -142,161 +142,120 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-
-
-
-@override
-Widget build(BuildContext context) {
-  return Container(
-    decoration: BoxDecoration(
-      border: Border.all(
-        color: Colors.orange,
-        width: 10.0,
-      ),
-    ),
-    child: Scaffold(
-      appBar: AppBar(
-        title: Text('Gestión de Eventos'),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.orange,
-                        width: 3.0,
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: TextFormField(
-                      controller: _eventNameController,
-                      decoration: InputDecoration(labelText: 'Nombre del Evento'),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Por favor, ingresa un nombre para el evento.';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.orange,
-                        width: 3.0,
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: TextFormField(
-                      controller: _descriptionController,
-                      decoration: InputDecoration(labelText: 'Descripción del Evento'),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Por favor, ingresa una descripción para el evento.';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 5),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.orange,
-                        width: 3.0,
-                      ),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: TextFormField(
-                      controller: _dateController,
-                      decoration: InputDecoration(labelText: 'Fecha del Evento'),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Por favor, ingresa una fecha para el evento.';
-                        }
-                        return null;
-                      },
-                      readOnly: true,
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      _addEvent(context.read<EventData>());
-                      Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => EventListScreen()),
-    );
-                    },
-                    child: Text('Agregar Evento'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => EventListScreen()),
-    );
-                    },
-                    child: Text('Ver lista de evento'),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Eventos Registrados:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Consumer<EventData>(
-              builder: (context, eventData, child) {
-                return ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: eventData.events.length,
-                  itemBuilder: (context, index) {
-                    return Card(
-                      margin: EdgeInsets.symmetric(vertical: 5),
-                      child: ListTile(
-                        title: Text('Nombre del Evento: ${eventData.events[index].name}'),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Descripción: ${eventData.events[index].description}'),
-                            Text('Fecha: ${eventData.events[index].date}'),
-                          ],
-                        ),
-                        onTap: () {
-                          String eventId = eventData.events[index].id;
-                          print(eventId);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  FamilyListScreen(),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ],
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.orange,
+          width: 10.0,
         ),
       ),
-    ),
-  );
-}
-
-
-
-
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Gestión de Eventos'),
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orange,
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextFormField(
+                        controller: _eventNameController,
+                        decoration:
+                            InputDecoration(labelText: 'Nombre del Evento'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Por favor, ingresa un nombre para el evento.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orange,
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextFormField(
+                        controller: _descriptionController,
+                        decoration: InputDecoration(
+                            labelText: 'Descripción del Evento'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Por favor, ingresa una descripción para el evento.';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.orange,
+                          width: 3.0,
+                        ),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextFormField(
+                        controller: _dateController,
+                        decoration:
+                            InputDecoration(labelText: 'Fecha del Evento'),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Por favor, ingresa una fecha para el evento.';
+                          }
+                          return null;
+                        },
+                        readOnly: true,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        _addEvent(context.read<EventData>());
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EventListScreen()),
+                        );
+                      },
+                      child: Text('Agregar Evento'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => EventListScreen()),
+                        );
+                      },
+                      child: Text('Ver lista de evento'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
